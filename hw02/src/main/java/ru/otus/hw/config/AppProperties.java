40a5441aa4b7304f1b23bf4c454e2f31f1be9f7a
Juraldinio <Juraldinio@gmail.com>
2026-03-31 21:51:30 +0300

    hw02 - wip

--- hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,8 +2,10 @@ package ru.otus.hw.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
     private int rightAnswersCountToPass;
@@ -11,8 +13,8 @@ public class AppProperties implements TestConfig, TestFileNameProvider {
     private String testFileName;
 
     public AppProperties(
-            @Value("${test.right.answers.count}") int rightAnswersCountToPass,
-            @Value("${test.file.name}") String testFileName
+            @Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass,
+            @Value("${test.fileName}") String testFileName
     ) {
         this.rightAnswersCountToPass = rightAnswersCountToPass;
         this.testFileName = testFileName;
