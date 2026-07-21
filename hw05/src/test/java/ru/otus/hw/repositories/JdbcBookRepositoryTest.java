db95b6c564015721091b099eb756980eed212260
Juraldinio <Juraldinio@gmail.com>
2026-07-21 22:07:18 +0300

    hw05: Switch to Liquibase. creation and fullfill by migration

--- hw05/src/test/java/ru/otus/hw/repositories/JdbcBookRepositoryTest.java
+++ hw05/src/test/java/ru/otus/hw/repositories/JdbcBookRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import ru.otus.hw.models.Author;
@@ -19,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Репозиторий на основе Jdbc для работы с книгами ")
 @JdbcTest
+@ImportAutoConfiguration(LiquibaseAutoConfiguration.class)
 @Import({JdbcBookRepository.class, JdbcGenreRepository.class})
 class JdbcBookRepositoryTest {
 
